3e5a8a64efc5c1fc4acc5f836e525a8cede54d91
Muhammad Syifa Ridhoni <syifaridhoni02@gmail.com>
2023-07-17 00:13:01 +0700

    Create BandNameGenerator.py
    Band Name Generator

---
+++ [DAY 1 PROJECT] BAND NAME GENERATOR/BandNameGenerator.py
@@ -0,0 +1,7 @@
+###### THIS CODE WILL GENERATE YOU A BAND NAME ######
+
+question_1 = input("What is your favorite food? ")
+question_2 = input("What is your favorite place? ")
+print("Your band name is " + question_1 + " " + question_2)
+
+###---DECOMOX---###
